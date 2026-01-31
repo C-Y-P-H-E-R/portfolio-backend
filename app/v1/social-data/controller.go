1e22851430218b274b2a13cb6b171a3a55d4f84f
C-Y-P-H-E-R <quasarkid123@gmail.com>
2026-01-31 16:01:26 +0530

    social data api added

---
+++ app/v1/social-data/controller.go
@@ -0,0 +1,30 @@
+package social_data
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func GET(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"status": http.StatusOK,
+		"message": "Success",
+		"data": map[string]interface{}{
+			"linkedin": map[string]string{
+				"url": "https://www.linkedin.com/in/kushagrasharmaofficial",
+				"username": "Kushagra Sharma",
+				"registered_email": "myselflks@yahoo.com",
+			},
+			"github": map[string]string{
+				"url": "https://github.com/C-Y-P-H-E-R",
+				"username": "C-Y-P-H-E-R",
+				"registered_email": "quasarkid123@gmail.com",
+			},
+			"leetcode": map[string]string{
+				"url": "https://leetcode.com/u/sololevelerIndia/",
+				"username": "sololevelerIndia",
+				"registered_email": "quasarkid123@gmail.com",
+			},
+		},
+	})
+}
--- config/router.go
+++ config/router.go
@@ -2,7 +2,7 @@ package config
 
 import (
 	"portfolio-backend/app/v1/health"
-
+	"portfolio-backend/app/v1/social-data"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,5 +10,6 @@ func SetupRouter(r *gin.Engine) {
 	v1 := r.Group("/go/api/v1")
 	{
 		v1.GET("/ping", health.Ping)
+		v1.GET("/socials", social_data.GET)
 	}
 }
