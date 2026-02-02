ef754e8bf50ca764058518c044587c2cfdac25db
C-Y-P-H-E-R <quasarkid123@gmail.com>
2026-02-02 20:31:22 +0530

    allowing cross origin requests

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"portfolio-backend/config"
-
+	"portfolio-backend/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
+	r.Use(middleware.CORSMiddleware())
 	config.SetupRouter(r)
 
 	r.Run(":8080")
